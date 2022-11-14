e95b9222da094cba1370e68b60e732f59efc5435
soodev20 <soodev20@gmail.com>
2022-11-14 13:16:59 +0900

    Add: club & filter types

--- src/type/type.ts
+++ src/type/type.ts
@@ -1,4 +1,4 @@
-interface iPerson {
+export interface iPerson {
   name: string;
 }
 
@@ -9,18 +9,24 @@ interface iMeeting {
 }
 
 export interface iClub extends iPerson {
-  id: string
-  type: string
-  place: string
-  coverUrl: string
-  meetings: Array<iMeeting>
-  description: string
+  id: string;
+  type: string;
+  place: string;
+  coverUrl: string;
+  meetings: Array<iMeeting>;
+  description: string;
 }
 
 export interface iResponseClubInfo {
   club: iClub;
-  price: number
-  leaders: Array<iPerson>
-  partners: Array<iPerson>,
-  createdAt: string
+  price: number;
+  leaders: Array<iPerson>;
+  partners: Array<iPerson>;
+  createdAt: string;
+}
+
+export interface iFilterCondition {
+  type: Array<string>;
+  place: Array<string>;
+  price: { lower: number, upper: number };
 }
