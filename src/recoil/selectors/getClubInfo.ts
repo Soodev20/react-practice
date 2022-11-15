ff7b76a9b1a06a1e59b3000310e8fcfc366ce138
soodev20 <soodev20@gmail.com>
2022-11-16 00:50:41 +0900

    Change: mock data && calling mock data

--- src/recoil/selectors/getClubInfo.ts
+++ src/recoil/selectors/getClubInfo.ts
@@ -1,13 +1,18 @@
 import { selector } from 'recoil';
 import axiosInstance from '../../api/axios';
+import mockData from '../../static/exampleData.json';
 import { iResponseClubInfo } from '../../type/type';
 
 const getClubInfoSelector = selector<Array<iResponseClubInfo>>({
   key:  '@clubInfo/getClubInfo',
   get: async () => {
-    const response = await axiosInstance.get('');
+    try {
+      const response = await axiosInstance.get('');
 
-    return response.data;
+      if (response.status === 200) return response.data;
+    } catch (e) {
+      return mockData;
+    }
   },
 });
 
