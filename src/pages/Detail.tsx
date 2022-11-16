9e641200d9f46f4dde38bb8cf8eb6218aba68e49
soodev20 <soodev20@gmail.com>
2022-11-16 12:47:24 +0900

    Feat: Detail page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,13 +1,29 @@
-import { useRecoilValue } from 'recoil';
-import getClubInfoSelector from '../recoil/selectors/getClubInfo';
+import { Fragment, useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { useLocation } from 'react-router-dom';
+import clubInfoIdAtom from '../recoil/atoms/clubInfoId';
+import ClubDetailCard from '../components/ClubDetailCard';
+import clubDetailSelector from '../recoil/selectors/clubDetail';
+import { iResponseClubInfo } from '../type/type';
 
 function DetailPage() {
-  const result = useRecoilValue(getClubInfoSelector);
+  const [id, setId] = useRecoilState<string>(clubInfoIdAtom);
+  const designatedClubData = useRecoilValue<iResponseClubInfo | undefined>(clubDetailSelector);
+  const location = useLocation();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const idParams = searchParams.get('id');
+
+    if (!idParams) return;
+
+    setId(idParams);
+  }, [])
 
   return (
-    <div>
-      <p>This is DetailPage</p>
-    </div>
+    <Fragment>
+      {id.length && !!designatedClubData && <ClubDetailCard cardData={designatedClubData}/>}
+    </Fragment>
   );
 }
 
