a930770cbee66256d3bc179776108740e293ccb3
soodev20 <soodev20@gmail.com>
2022-11-16 15:52:06 +0900

    Mid-save: infinite scroll trial

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from 'react';
+import { Fragment, useEffect, useState, useRef, RefObject, useCallback } from 'react';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { useLocation } from 'react-router-dom';
 import { css } from '@emotion/react';
@@ -16,7 +16,11 @@ const gridWrapper = css`
 function ListPage() {
   const result = useRecoilValue<Array<iResponseClubInfo> | undefined>(filterClubInfoSelector);
   const [options, setOptions] = useRecoilState<iFilterCondition>(filterOptionChoiceAtom);
+  const [data, setData] = useState<Array<iResponseClubInfo>>([]);
+  const [page, setPage] = useState<number>(0);
+  const observer = useRef<IntersectionObserver | null>(null)
   const location = useLocation();
+  const CONTENT_PER_PAGE = 10;
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -30,17 +34,47 @@ function ListPage() {
     if (placeParams) newOption.place = [placeParams];
 
     setOptions({ ...options, ...newOption});
-  }, [])
+  }, []);
+
+  const lastItemRef = useCallback((node: Element) => {
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        setPage(prevPage => prevPage + 1)
+      }
+    });
+    if (node) observer.current.observe(node);
+  }, [result])
+
+  useEffect(() => {
+    if (result && result.length > CONTENT_PER_PAGE) {
+      const startPoint = page ? CONTENT_PER_PAGE * page + 1 : CONTENT_PER_PAGE * page;
+      const lastPoint = (startPoint + CONTENT_PER_PAGE > result.length) ? -1 : startPoint + CONTENT_PER_PAGE;
+      const newData = result.slice(startPoint, lastPoint);
+
+      setData([...data, ...newData]);
+    }
+
+    if (result && result.length <= CONTENT_PER_PAGE) {
+      setData(result);
+    }
+  }, [result, page])
 
   return (
     <Fragment>
       <div css={gridWrapper}>
-        {!!result && result.map(cardData => {
-          return (
-            <div key={cardData.createdAt}>
-              <ClubInfoCard cardData={cardData}/>
-            </div>
-          )
+        {!!result && data.map((cardData, index) => {
+            if (result.length === index + 1) {
+              return (
+                <div key={cardData.createdAt} ref={() => lastItemRef}>
+                  <ClubInfoCard cardData={cardData}/>
+                </div>
+              )
+            } else {
+              <div key={cardData.createdAt}>
+                <ClubInfoCard cardData={cardData}/>
+              </div>
+            }
         })}
       </div>
     </Fragment>
